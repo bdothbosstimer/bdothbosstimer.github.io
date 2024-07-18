22d7e8eb451cde108c50e95f02bcce0711280d43
Fukki <95914828+bdothbosstimer@users.noreply.github.com>
2024-07-18 10:00:49 +0700

    :x

---
+++ js/boss-seapalace.js
@@ -0,0 +1,822 @@
+const bossTable = {
+	"0": {
+		"00:00:00": [
+			{
+				"type": "event-boss",
+				"name": "[Event]เบลล์น้อย",
+				"img": "imgs/vell.png"
+			}
+		],
+		"00:30:00": [
+			{
+				"type": "world-boss",
+				"name": "เทพแห่งการทุจริต คจาคาร์",
+				"img": "imgs/kzarka.png"
+			}
+		],
+		"06:00:00": [],
+		"10:00:00": [
+			{
+				"type": "world-boss",
+				"name": "นูเวอร์",
+				"img": "imgs/nouver.png"
+			},
+			{
+				"type": "world-boss",
+				"name": "คารานด้า",
+				"img": "imgs/karanda.png"
+			}
+		],
+		"11:00:00": [
+			{
+				"type": "event-boss",
+				"name": "[Event]เบลล์น้อย",
+				"img": "imgs/vell.png"
+			}
+		],
+		"12:00:00": [],
+		"13:00:00": [
+			{
+				"type": "world-boss",
+				"name": "กามอส",
+				"img": "imgs/garmoth.png"
+			}
+		],
+		"14:00:00": [
+			{
+				"type": "world-boss",
+				"name": "คูทุม",
+				"img": "imgs/kutum.png"
+			},
+			{
+				"type": "world-boss",
+				"name": "คารานด้า",
+				"img": "imgs/karanda.png"
+			}
+		],
+		"14:30:00": [
+			{
+				"type": "event-boss",
+				"name": "[Event]เบลล์น้อย",
+				"img": "imgs/vell.png"
+			}
+		],
+		"15:00:00": [
+			{
+				"type": "world-boss",
+				"name": "เบลล์",
+				"img": "imgs/vell.png"
+			}
+		],
+		"16:00:00": [],
+		"17:00:00": [
+			{
+				"type": "event-boss",
+				"name": "[Event]เบลล์น้อย",
+				"img": "imgs/vell.png"
+			}
+		],
+		"19:00:00": [
+			{
+				"type": "world-boss",
+				"name": "เทพแห่งการทุจริต คจาคาร์",
+				"img": "imgs/kzarka.png"
+			},
+			{
+				"type": "world-boss",
+				"name": "คารานด้า",
+				"img": "imgs/karanda.png"
+			}
+		],
+		"20:00:00": [
+			{
+				"type": "war-node",
+				"name": "สงครามฐาน",
+				"img": "imgs/warnode.png"
+			}
+		],
+		"22:15:00": [
+			{
+				"type": "world-boss",
+				"name": "กามอส",
+				"img": "imgs/garmoth.png"
+			}
+		],
+		"22:45:00": [
+			{
+				"type": "event-boss",
+				"name": "[Event]เบลล์น้อย",
+				"img": "imgs/vell.png"
+			}
+		],
+		"23:00:00": [
+			{
+				"type": "world-boss",
+				"name": "คูทุม",
+				"img": "imgs/kutum.png"
+			},
+			{
+				"type": "world-boss",
+				"name": "นูเวอร์",
+				"img": "imgs/nouver.png"
+			}
+		],
+		"23:30:00": [],
+		"23:45:00": []
+	},
+	"1": {
+		"00:00:00": [
+			{
+				"type": "event-boss",
+				"name": "[Event]เบลล์น้อย",
+				"img": "imgs/vell.png"
+			}
+		],
+		"00:30:00": [
+			{
+				"type": "world-boss",
+				"name": "คูทุม",
+				"img": "imgs/kutum.png"
+			}
+		],
+		"06:00:00": [],
+		"10:00:00": [
+			{
+				"type": "world-boss",
+				"name": "เทพแห่งการทุจริต คจาคาร์",
+				"img": "imgs/kzarka.png"
+			},
+			{
+				"type": "world-boss",
+				"name": "นูเวอร์",
+				"img": "imgs/nouver.png"
+			}
+		],
+		"11:00:00": [
+			{
+				"type": "event-boss",
+				"name": "[Event]เบลล์น้อย",
+				"img": "imgs/vell.png"
+			}
+		],
+		"12:00:00": [],
+		"13:00:00": [
+			{
+				"type": "world-boss",
+				"name": "กามอส",
+				"img": "imgs/garmoth.png"
+			}
+		],
+		"14:00:00": [
+			{
+				"type": "world-boss",
+				"name": "คูทุม",
+				"img": "imgs/kutum.png"
+			},
+			{
+				"type": "world-boss",
+				"name": "นูเวอร์",
+				"img": "imgs/nouver.png"
+			}
+		],
+		"14:30:00": [
+			{
+				"type": "event-boss",
+				"name": "[Event]เบลล์น้อย",
+				"img": "imgs/vell.png"
+			}
+		],
+		"15:00:00": [],
+		"16:00:00": [],
+		"17:00:00": [
+			{
+				"type": "event-boss",
+				"name": "[Event]เบลล์น้อย",
+				"img": "imgs/vell.png"
+			}
+		],
+		"19:00:00": [
+			{
+				"type": "world-boss",
+				"name": "เทพแห่งการทุจริต คจาคาร์",
+				"img": "imgs/kzarka.png"
+			},
+			{
+				"type": "world-boss",
+				"name": "คารานด้า",
+				"img": "imgs/karanda.png"
+			}
+		],
+		"20:00:00": [
+			{
+				"type": "war-node",
+				"name": "สงครามฐาน",
+				"img": "imgs/warnode.png"
+			}
+		],
+		"22:15:00": [
+			{
+				"type": "world-boss",
+				"name": "กามอส",
+				"img": "imgs/garmoth.png"
+			}
+		],
+		"22:45:00": [
+			{
+				"type": "event-boss",
+				"name": "[Event]เบลล์น้อย",
+				"img": "imgs/vell.png"
+			}
+		],
+		"23:00:00": [
+			{
+				"type": "world-boss",
+				"name": "ผู้ทำลายแห่งมีลม็อก โอฟิน",
+				"img": "imgs/offin.png"
+			}
+		],
+		"23:30:00": [],
+		"23:45:00": []
+	},
+	"2": {
+		"00:00:00": [
+			{
+				"type": "event-boss",
+				"name": "[Event]เบลล์น้อย",
+				"img": "imgs/vell.png"
+			}
+		],
+		"00:30:00": [
+			{
+				"type": "world-boss",
+				"name": "นูเวอร์",
+				"img": "imgs/nouver.png"
+			}
+		],
+		"06:00:00": [],
+		"10:00:00": [
+			{
+				"type": "world-boss",
+				"name": "คูทุม",
+				"img": "imgs/kutum.png"
+			},
+			{
+				"type": "world-boss",
+				"name": "คารานด้า",
+				"img": "imgs/karanda.png"
+			}
+		],
+		"11:00:00": [
+			{
+				"type": "event-boss",
+				"name": "[Event]เบลล์น้อย",
+				"img": "imgs/vell.png"
+			}
+		],
+		"12:00:00": [],
+		"13:00:00": [
+			{
+				"type": "world-boss",
+				"name": "กามอส",
+				"img": "imgs/garmoth.png"
+			}
+		],
+		"14:00:00": [
+			{
+				"type": "world-boss",
+				"name": "คูทุม",
+				"img": "imgs/kutum.png"
+			},
+			{
+				"type": "world-boss",
+				"name": "เทพแห่งการทุจริต คจาคาร์",
+				"img": "imgs/kzarka.png"
+			}
+		],
+		"14:30:00": [
+			{
+				"type": "event-boss",
+				"name": "[Event]เบลล์น้อย",
+				"img": "imgs/vell.png"
+			}
+		],
+		"15:00:00": [],
+		"16:00:00": [],
+		"17:00:00": [
+			{
+				"type": "event-boss",
+				"name": "[Event]เบลล์น้อย",
+				"img": "imgs/vell.png"
+			}
+		],
+		"19:00:00": [
+			{
+				"type": "world-boss",
+				"name": "เทพแห่งการทุจริต คจาคาร์",
+				"img": "imgs/kzarka.png"
+			},
+			{
+				"type": "world-boss",
+				"name": "คารานด้า",
+				"img": "imgs/karanda.png"
+			}
+		],
+		"20:00:00": [
+			{
+				"type": "war-node",
+				"name": "สงครามฐาน",
+				"img": "imgs/warnode.png"
+			}
+		],
+		"22:15:00": [
+			{
+				"type": "world-boss",
+				"name": "กามอส",
+				"img": "imgs/garmoth.png"
+			}
+		],
+		"22:45:00": [
+			{
+				"type": "event-boss",
+				"name": "[Event]เบลล์น้อย",
+				"img": "imgs/vell.png"
+			}
+		],
+		"23:00:00": [
+			{
+				"type": "world-boss",
+				"name": "คูทุม",
+				"img": "imgs/kutum.png"
+			}
+		],
+		"23:30:00": [],
+		"23:45:00": []
+	},
+	"3": {
+		"00:00:00": [
+			{
+				"type": "event-boss",
+				"name": "[Event]เบลล์น้อย",
+				"img": "imgs/vell.png"
+			}
+		],
+		"00:30:00": [
+			{
+				"type": "world-boss",
+				"name": "คารานด้า",
+				"img": "imgs/karanda.png"
+			}
+		],
+		"06:00:00": [],
+		"10:00:00": [
+			{
+				"type": "world-boss",
+				"name": "เทพแห่งการทุจริต คจาคาร์",
+				"img": "imgs/kzarka.png"
+			},
+			{
+				"type": "world-boss",
+				"name": "นูเวอร์",
+				"img": "imgs/nouver.png"
+			}
+		],
+		"11:00:00": [
+			{
+				"type": "event-boss",
+				"name": "[Event]เบลล์น้อย",
+				"img": "imgs/vell.png"
+			}
+		],
+		"12:00:00": [],
+		"13:00:00": [
+			{
+				"type": "world-boss",
+				"name": "กามอส",
+				"img": "imgs/garmoth.png"
+			}
+		],
+		"14:00:00": [
+			{
+				"type": "world-boss",
+				"name": "คูทุม",
+				"img": "imgs/kutum.png"
+			},
+			{
+				"type": "world-boss",
+				"name": "คารานด้า",
+				"img": "imgs/karanda.png"
+			}
+		],
+		"14:30:00": [
+			{
+				"type": "event-boss",
+				"name": "[Event]เบลล์น้อย",
+				"img": "imgs/vell.png"
+			}
+		],
+		"15:00:00": [],
+		"16:00:00": [],
+		"17:00:00": [
+			{
+				"type": "event-boss",
+				"name": "[Event]เบลล์น้อย",
+				"img": "imgs/vell.png"
+			}
+		],
+		"19:00:00": [
+			{
+				"type": "world-boss",
+				"name": "เทพแห่งการทุจริต คจาคาร์",
+				"img": "imgs/kzarka.png"
+			},
+			{
+				"type": "world-boss",
+				"name": "คารานด้า",
+				"img": "imgs/karanda.png"
+			}
+		],
+		"20:00:00": [
+			{
+				"type": "war-node",
+				"name": "สงครามฐาน",
+				"img": "imgs/warnode.png"
+			}
+		],
+		"22:15:00": [
+			{
+				"type": "world-boss",
+				"name": "กามอส",
+				"img": "imgs/garmoth.png"
+			}
+		],
+		"22:45:00": [
+			{
+				"type": "event-boss",
+				"name": "[Event]เบลล์น้อย",
+				"img": "imgs/vell.png"
+			}
+		],
+		"23:00:00": [
+			{
+				"type": "world-boss",
+				"name": "คูทุม",
+				"img": "imgs/kutum.png"
+			},
+			{
+				"type": "world-boss",
+				"name": "นูเวอร์",
+				"img": "imgs/nouver.png"
+			}
+		],
+		"23:30:00": [],
+		"23:45:00": []
+	},
+	"4": {
+		"00:00:00": [
+			{
+				"type": "event-boss",
+				"name": "[Event]เบลล์น้อย",
+				"img": "imgs/vell.png"
+			}
+		],
+		"00:30:00": [
+			{
+				"type": "world-boss",
+				"name": "นูเวอร์",
+				"img": "imgs/nouver.png"
+			}
+		],
+		"06:00:00": [],
+		"10:00:00": [
+			{
+				"type": "world-boss",
+				"name": "เทพแห่งการทุจริต คจาคาร์",
+				"img": "imgs/kzarka.png"
+			},
+			{
+				"type": "world-boss",
+				"name": "คารานด้า",
+				"img": "imgs/karanda.png"
+			}
+		],
+		"11:00:00": [
+			{
+				"type": "event-boss",
+				"name": "[Event]เบลล์น้อย",
+				"img": "imgs/vell.png"
+			}
+		],
+		"12:00:00": [],
+		"13:00:00": [
+			{
+				"type": "world-boss",
+				"name": "กามอส",
+				"img": "imgs/garmoth.png"
+			}
+		],
+		"14:00:00": [
+			{
+				"type": "world-boss",
+				"name": "คูทุม",
+				"img": "imgs/kutum.png"
+			},
+			{
+				"type": "world-boss",
+				"name": "นูเวอร์",
+				"img": "imgs/nouver.png"
+			}
+		],
+		"14:30:00": [
+			{
+				"type": "event-boss",
+				"name": "[Event]เบลล์น้อย",
+				"img": "imgs/vell.png"
+			}
+		],
+		"15:00:00": [],
+		"16:00:00": [],
+		"17:00:00": [
+			{
+				"type": "event-boss",
+				"name": "[Event]เบลล์น้อย",
+				"img": "imgs/vell.png"
+			}
+		],
+		"19:00:00": [
+			{
+				"type": "world-boss",
+				"name": "เทพแห่งการทุจริต คจาคาร์",
+				"img": "imgs/kzarka.png"
+			},
+			{
+				"type": "world-boss",
+				"name": "คารานด้า",
+				"img": "imgs/karanda.png"
+			}
+		],
+		"20:00:00": [
+			{
+				"type": "war-node",
+				"name": "สงครามฐาน",
+				"img": "imgs/warnode.png"
+			}
+		],
+		"22:15:00": [
+			{
+				"type": "world-boss",
+				"name": "กามอส",
+				"img": "imgs/garmoth.png"
+			}
+		],
+		"22:45:00": [
+			{
+				"type": "event-boss",
+				"name": "[Event]เบลล์น้อย",
+				"img": "imgs/vell.png"
+			}
+		],
+		"23:00:00": [
+			{
+				"type": "world-boss",
+				"name": "ผู้ทำลายแห่งมีลม็อก โอฟิน",
+				"img": "imgs/offin.png"
+			}
+		],
+		"23:30:00": [],
+		"23:45:00": []
+	},
+	"5": {
+		"00:00:00": [
+			{
+				"type": "event-boss",
+				"name": "[Event]เบลล์น้อย",
+				"img": "imgs/vell.png"
+			}
+		],
+		"00:30:00": [
+			{
+				"type": "world-boss",
+				"name": "เทพแห่งการทุจริต คจาคาร์",
+				"img": "imgs/kzarka.png"
+			}
+		],
+		"06:00:00": [],
+		"10:00:00": [
+			{
+				"type": "world-boss",
+				"name": "คูทุม",
+				"img": "imgs/kutum.png"
+			},
+			{
+				"type": "world-boss",
+				"name": "คารานด้า",
+				"img": "imgs/karanda.png"
+			}
+		],
+		"11:00:00": [
+			{
+				"type": "event-boss",
+				"name": "[Event]เบลล์น้อย",
+				"img": "imgs/vell.png"
+			}
+		],
+		"12:00:00": [],
+		"13:00:00": [
+			{
+				"type": "world-boss",
+				"name": "กามอส",
+				"img": "imgs/garmoth.png"
+			}
+		],
+		"14:00:00": [
+			{
+				"type": "world-boss",
+				"name": "คูทุม",
+				"img": "imgs/kutum.png"
+			},
+			{
+				"type": "world-boss",
+				"name": "เทพแห่งการทุจริต คจาคาร์",
+				"img": "imgs/kzarka.png"
+			}
+		],
+		"14:30:00": [
+			{
+				"type": "event-boss",
+				"name": "[Event]เบลล์น้อย",
+				"img": "imgs/vell.png"
+			}
+		],
+		"15:00:00": [],
+		"16:00:00": [],
+		"17:00:00": [
+			{
+				"type": "event-boss",
+				"name": "[Event]เบลล์น้อย",
+				"img": "imgs/vell.png"
+			}
+		],
+		"19:00:00": [
+			{
+				"type": "world-boss",
+				"name": "เทพแห่งการทุจริต คจาคาร์",
+				"img": "imgs/kzarka.png"
+			},
+			{
+				"type": "world-boss",
+				"name": "คารานด้า",
+				"img": "imgs/karanda.png"
+			}
+		],
+		"20:00:00": [
+			{
+				"type": "war-node",
+				"name": "สงครามฐาน",
+				"img": "imgs/warnode.png"
+			}
+		],
+		"22:15:00": [
+			{
+				"type": "world-boss",
+				"name": "กามอส",
+				"img": "imgs/garmoth.png"
+			}
+		],
+		"22:45:00": [
+			{
+				"type": "event-boss",
+				"name": "[Event]เบลล์น้อย",
+				"img": "imgs/vell.png"
+			}
+		],
+		"23:00:00": [
+			{
+				"type": "world-boss",
+				"name": "คูทุม",
+				"img": "imgs/kutum.png"
+			}
+		],
+		"23:30:00": [],
+		"23:45:00": []
+	},
+	"6": {
+		"00:00:00": [
+			{
+				"type": "event-boss",
+				"name": "[Event]เบลล์น้อย",
+				"img": "imgs/vell.png"
+			}
+		],
+		"00:30:00": [
+			{
+				"type": "world-boss",
+				"name": "นูเวอร์",
+				"img": "imgs/nouver.png"
+			}
+		],
+		"06:00:00": [],
+		"10:00:00": [
+			{
+				"type": "world-boss",
+				"name": "เทพแห่งการทุจริต คจาคาร์",
+				"img": "imgs/kzarka.png"
+			},
+			{
+				"type": "world-boss",
+				"name": "คารานด้า",
+				"img": "imgs/karanda.png"
+			}
+		],
+		"11:00:00": [
+			{
+				"type": "event-boss",
+				"name": "[Event]เบลล์น้อย",
+				"img": "imgs/vell.png"
+			}
+		],
+		"12:00:00": [],
+		"13:00:00": [
+			{
+				"type": "world-boss",
+				"name": "กามอส",
+				"img": "imgs/garmoth.png"
+			}
+		],
+		"14:00:00": [
+			{
+				"type": "world-boss",
+				"name": "คูทุม",
+				"img": "imgs/kutum.png"
+			},
+			{
+				"type": "world-boss",
+				"name": "นูเวอร์",
+				"img": "imgs/nouver.png"
+			}
+		],
+		"14:30:00": [
+			{
+				"type": "event-boss",
+				"name": "[Event]เบลล์น้อย",
+				"img": "imgs/vell.png"
+			}
+		],
+		"15:00:00": [
+			{
+				"type": "world-boss",
+				"name": "เบลล์",
+				"img": "imgs/vell.png"
+			}
+		],
+		"16:00:00": [],
+		"17:00:00": [
+			{
+				"type": "event-boss",
+				"name": "[Event]เบลล์น้อย",
+				"img": "imgs/vell.png"
+			}
+		],
+		"19:00:00": [
+			{
+				"type": "world-boss",
+				"name": "เทพแห่งการทุจริต คจาคาร์",
+				"img": "imgs/kzarka.png"
+			},
+			{
+				"type": "world-boss",
+				"name": "คารานด้า",
+				"img": "imgs/karanda.png"
+			}
+		],
+		"20:00:00": [
+			{
+				"type": "war-node",
+				"name": "สงครามฐาน",
+				"img": "imgs/warnode.png"
+			}
+		],
+		"22:15:00": [
+			{
+				"type": "world-boss",
+				"name": "กามอส",
+				"img": "imgs/garmoth.png"
+			}
+		],
+		"22:45:00": [
+			{
+				"type": "event-boss",
+				"name": "[Event]เบลล์น้อย",
+				"img": "imgs/vell.png"
+			}
+		],
+		"23:00:00": [
+			{
+				"type": "world-boss",
+				"name": "คูทุม",
+				"img": "imgs/kutum.png"
+			}
+		],
+		"23:30:00": [],
+		"23:45:00": []
+	}
+}
